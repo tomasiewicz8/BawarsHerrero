e6e0060501b64163803004c67eb2c4fb94c837b9
tomasiewicz8 <tomasiewic@gmail.com>
2023-09-15 18:14:31 +0200

    codigo nuevo

--- galeria.js
+++ galeria.js
@@ -1,1 +1,16 @@
-wgsdg
+$(document).ready(function() {
+    // Inicializa FancyBox
+    $('[data-fancybox="gallery"]').fancybox({
+        buttons: [
+            "zoom",
+            "share",
+            "slideShow",
+            "fullScreen",
+            "download",
+            "thumbs",
+            "close"
+        ],
+        loop: true, // Permite la navegación en bucle
+        keyboard: true // Habilita la navegación con el teclado
+    });
+});
